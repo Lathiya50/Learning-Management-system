0aa128391b911efeb4cc1c308afedd7fe7aa4194
Harmik Lathiya <lathiyaharmik50@gmail.com>
2023-09-25 00:20:14 +0530

    added User registration and activation routes,middleware and controler

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,9 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import cookieParser from "cookie-parser";
 export const app = express(); 
+import userRouter from './routes/user.route';
+ 
+
 
 //body parser
 app.use(express.json({ limit: "50mb" }))
@@ -13,6 +16,11 @@ app.use(cookieParser());
 
 //cors => cross orign resouces sharing
 app.use(cors({ origin: process.env.ORIGIN }))
+
+
+//routes
+app.use("/api/v1", userRouter);
+
 
 //testing API
 app.get("/test", (req:Request,res:Response,next:NextFunction) => {
