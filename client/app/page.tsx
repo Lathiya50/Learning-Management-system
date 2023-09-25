ea470f9d653d4a5f14a7babc72b86a6f9eb92905
Shubhampatel136 <mangroliyashubham@gmail.com>
2023-09-25 14:53:53 +0530

    Second commit

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,17 @@
 'use client'
 import React, {FC,useState} from "react";
+import Heading from "./utils/Heading";
 
 interface props{}
 
 const Page: FC<props>= (props)=>{
   return (
     <div>
-      
+      <Heading 
+        title="ELearning"
+        description="ELearning is a platform for studdents to learn and get help from teachers"
+        keywords="Programing,MERN,Redux,Machine Learning"
+      />
     </div>
   )
 };
--- client/app/utils/Heading.tsx
+++ client/app/utils/Heading.tsx
@@ -10,6 +10,11 @@
     return(
         <>
             <title>{title}</title>
+            <meta name="description" content={description}/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="keywords" content={keywords}/>
         </>
-    )
- }
+    );
+ };
+
+ export default Heading;
