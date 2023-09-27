44f7b8eee6a03e8f66f3103dddb9aa94d67c28aa
Shubhampatel136 <mangroliyashubham@gmail.com>
2023-09-27 14:49:20 +0530

    third commit

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,19 +1,19 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import {Poppins} from "next/font/google";
-import { Josefin_Sans  } from 'next/font/google';
+import { Poppins } from "next/font/google";
+import { Josefin_Sans } from 'next/font/google';
 import { ThemeProvider } from './utils/theme-provider';
-  
+
 const poppins = Poppins({
-  subsets:["latin"],
-  weight:["400","500","600","700"],
-  variable:"--font-Poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-Poppins",
 });
 
-const josefin= Josefin_Sans({
-  subsets:["latin"],
-  weight:["400","500","600","700"],
-  variable:"--font-Josefin",
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-Josefin",
 });
 
 export default function RootLayout({
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} ${josefin.variable} !bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}>
+      <body className={`${poppins.variable} ${josefin.variable} !bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}>
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
           {children}
         </ThemeProvider>
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,16 +1,24 @@
 'use client'
-import React, {FC,useState} from "react";
+import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
+import Header from "./components/Header";
 
-interface props{}
+interface props { }
 
-const Page: FC<props>= (props)=>{
+const Page: FC<props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
   return (
     <div>
-      <Heading 
+      <Heading
         title="ELearning"
         description="ELearning is a platform for studdents to learn and get help from teachers"
         keywords="Programing,MERN,Redux,Machine Learning"
+      />
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
       />
     </div>
   )
