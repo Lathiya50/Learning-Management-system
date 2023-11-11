6af3b8fd5ba7e5ccd7c5d780193428596af268fa
Harmik Lathiya <lathiyaharmik50@gmail.com>
2023-11-11 18:37:08 +0530

    modified the origin congif

--- server/app.ts
+++ server/app.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
     cors({
-        origin: ['http://localhost:3000'],
+        origin: process.env.ORIGIN,
         credentials: true,
     })
 );
